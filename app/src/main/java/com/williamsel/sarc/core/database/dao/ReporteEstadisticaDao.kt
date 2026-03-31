93196bfab8254c53f3b11265ca19dd2a1a1e7c45
Williams-Espinosa <w.espinosa.it@gmail.com>
2026-03-30 23:40:37 -0600

    first commit

--- app/src/main/java/com/williamsel/sarc/core/database/dao/ReporteEstadisticaDao.kt
+++
@@ -1,4 +0,0 @@
-package com.williamsel.sarc.core.database.dao
-
-class ReporteEstadisticaDao {
-}
--- app/src/main/java/com/williamsel/sarc/core/database/entities/teEstadisticaEntity.kt
+++
@@ -1,4 +0,0 @@
-package com.williamsel.sarc.core.database.entities
-
-class teEstadisticaEntity {
-}
