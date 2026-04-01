6870b01db3f4b3558f1653c3d705705147bc5f8c
Williams-Espinosa <w.espinosa.it@gmail.com>
2026-03-31 19:06:15 -0600

    feat: actualización general del proyecto

--- app/src/main/java/com/williamsel/sarc/ui/theme/Theme.kt
+++ app/src/main/java/com/williamsel/sarc/ui/theme/Theme.kt
@@ -9,7 +9,6 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
-
 private val LightColorScheme = lightColorScheme(
     primary          = SarcGreen,
     onPrimary        = SurfaceWhite,
@@ -25,6 +24,7 @@ private val LightColorScheme = lightColorScheme(
     error            = ErrorRed,
     onError          = SurfaceWhite
 )
+
 private val DarkColorScheme = darkColorScheme(
     primary          = SarcGreen,
     onPrimary        = DarkNavy,
@@ -41,10 +41,9 @@ private val DarkColorScheme = darkColorScheme(
     onError          = SurfaceWhite
 )
 
-
 @Composable
 fun SarcTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
+    darkTheme: Boolean    = isSystemInDarkTheme(),
     dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
